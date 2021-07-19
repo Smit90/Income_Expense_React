873f67812f065d796ea924b1953f5c90aff71e37
Smit Patel <45687440+Smit90@users.noreply.github.com>
2021-07-19 11:21:34 +0530

    add transaction

--- src/context/AppReducer.js
+++ src/context/AppReducer.js
@@ -1,5 +1,10 @@
 export const AppReducer = (state, action) => {
   switch (action.type) {
+    case "ADD_TRANSACTION":
+      return {
+        ...state,
+        transactions: [action.playload, ...state.transactions]
+      }
     case "DELETE_TRANSACTION":
       return {
         ...state,
